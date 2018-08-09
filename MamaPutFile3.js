1fd47290fa3227645e2b95d1f2fbace2b9216356
Benedict <Benedictiknkeonye@gmail.com>
2018-08-10 00:21:09 +0100

    My First JavaScript Project
    So these three files are supposed to come up with a page that displays the bill for a customer who purchased food via an online app, that's the idea. This page is basically the payment page and I would really appreciate all the help I can get. Thanks.

---
+++ MamaPutFile3.js
@@ -0,0 +1,53 @@
+/* Creation Of Variables */
+
+//Create variable for the welcome message
+var greeting='How you dey? ';
+var name='Alex';
+var message = ', abeg shook eye for wetin you buy:';
+
+//Concatenate the three variables above to create the welcome message
+var welcome= greeting + name + message;
+
+//Create variables for the code
+var code= 'RicePlantainChicken_';
+var item= code.length;
+var subTotal= item * 30;
+var delivery= 50;
+var vat= 20; 
+var grandTotal= subTotal + delivery + vat;
+
+
+
+/* Updating the old variables in HTML with new variables formed above */
+
+
+//Get element that has an id of greeting & update content
+var el= document.getElementById('greeting');
+el.textContent= welcome;
+
+//Get element that has id of usercode & update content
+var elCode= document.getElementById('usercode');
+elCode.textContent= code;
+
+//Get element that has id of items & update content
+var el= document.getElementById('items');
+elItem.textContent= item;
+
+//Get element that has id of subTotal & update content
+var el= document.getElementById('subTotal');
+elSubTotal.textContent = '$' + subTotal;
+
+//Get element that has id of delivery & update content
+var el= document.getElementById('delivery');
+elDelivery.textContent = '$' + delivery;
+
+//Get element that has id of vat & update content
+var el= document.getElementById('vat');
+elVat.textContent = '$' + vat;
+
+//Get element that has id of grandTotal & update content
+var el= document.getElementById('grandTotal');
+elGrandTotal.textContent = '$' + grandTotal;
+
+
+/* End Of JavaScript Code */
